e01b505331ed5daeca2e49a0fbefce98ae908f85
Joe Chang <joechang@Joede-MacBook-Pro.local>
2020-05-04 19:51:56 +0800

    add 39

--- leetcode_39.cpp
+++ leetcode_39.cpp
@@ -5,25 +5,28 @@ class Solution39 {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
+        vector<int> tempVec;
+        solution(candidates, target, 0, tempVec, res);
 
-        int divVal = 0;
-        int restVal = 0;
-        for (int i = candidates.size()-1; i >= 0; i--)
-        {
-            divVal = target / candidates[i];
-            restVal = target % candidates[i];
-
-            if (restVal == 0)
-            {
-                res.push_back(vector<int>(divVal,candidates[i]));
-            }
-            
-        }
-        
         return res;
     }
 
-    void smallerSet(vector<int> inputVec, int target) {
+    void solution(vector<int>& candidates, int target, int startIndex, vector<int> tempVec, vector<vector<int>>& res) {
         
+        if (target == 0)
+        {
+            res.push_back(tempVec);
+            return;
+        }
+        
+        for (int i = startIndex; i < candidates.size(); i++)
+        {
+            if (candidates[i] <= target)
+            {
+                tempVec.push_back(candidates[i]);
+                solution(candidates, target-candidates[i],i,tempVec,res);
+                tempVec.pop_back();
+            }
+        }
     }
 };
